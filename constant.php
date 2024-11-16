997c6439e148eae97b0b133efd3e96b0427913a7
Debkanta Dey <debkantadey832@gmail.com>
2024-11-16 18:57:17 +0530

    first commit

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6LcgH6kkAAAAALW_Qj3Sg2U9XJRFN8IgusrGPhLN"); 
+define('SECRET_KEY',"6LcgH6kkAAAAAAqTrMT_EfsLc0OAqnyatPvVekbH");
+?>
